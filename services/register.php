dfa6bce4683d973e31971f13bc3e1be63732f55f
Gianluca Dias De Micheli <de.micheli.gianluca@gmail.com>
2022-04-21 22:42:53 -0300

    Update register function

--- services/register.php
+++ services/register.php
@@ -1,4 +1,6 @@
 <?php
+    session_start();
+    
     include_once "requestAPI.php";
 
     if (isset($_POST['login'])){
@@ -12,15 +14,15 @@
 
         $response = requestApi('POST', 'http:/localhost:8080/login', $postData);
 
-        if ($response == "Internal Server Error"){
-            echo "<script> window.location = '../login.php?response=unauthorized' </script>";
-        } else {
-            session_cache_limiter('private');
-            session_cache_expire(60);
-            session_start();
+        if (strcmp($response, "OK")){
+            // session_cache_limiter('private');
+            // session_cache_expire(60);
+            
             $_SESSION['token'] = $response;
             $_SESSION['name'] = json_decode(requestApi('GET', 'http:/localhost:8080/dentists/' . $user, false, $_SESSION['token']))->name;
             echo "<script> window.location = '../index.php' </script>";
+        } else {
+            echo "<script> window.location = '../login.php?response=unauthorized' </script>";
         }
 
     } else {
@@ -30,14 +32,28 @@
     
             $postData = array (
                 "title" => $procedureTitle,
-                "description" => $procedureDescription,
-                "status" => true
+                "description" => $procedureDescription
             );
     
             $response = requestApi('POST', 'http:/localhost:8080/procedures/', $postData, $_SESSION['token']);
+
+            echo "<script> window.location = '../procedure-list.php?response=" . $response . "' </script>";
+        } else {
+            if (isset($_POST['updateProcedure'])){
+                $procedureId = isset($_POST['id']) ? $_POST['id'] : null;
+                $procedureTitle = isset($_POST['title']) ? $_POST['title'] : null;
+                $procedureDescription = isset($_POST['description']) ? $_POST['description'] : null;
+        
+                $postData = array (
+                    "id" => $procedureId,
+                    "title" => $procedureTitle,
+                    "description" => $procedureDescription
+                );
+        
+                $response = requestApi('POST', 'http:/localhost:8080/procedures/', $postData, $_SESSION['token']);
     
-    
-            echo "<script> window.location = '../procedure-list.php?response=ok' </script>";
+                echo "<script> window.location = '../procedure-list.php?response=" . $response . "' </script>";
+            }
         }
     }
 ?>
