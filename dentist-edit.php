0a318429d573733d93223b79128cdf6d6c0cd635
Gianluca Dias De Micheli <de.micheli.gianluca@gmail.com>
2022-04-22 17:39:50 -0300

    Create dentist function

--- dentist-edit.php
+++ dentist-edit.php
@@ -116,54 +116,61 @@
 
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Cadastrar Dentista</h1>
+                        <h1 class="h3 mb-0 text-gray-800">Atualizar Dentista</h1>
                     </div>
 
                     <!-- Content Row -->
                     <div class="row mb-4 p-4">
+
+                     <?php
+                        $name = isset($_GET['name']) ? $_GET['name'] : null;
+                        $surname = isset($_GET['surname']) ? $_GET['surname'] : null;
+                        $document = isset($_GET['document']) ? $_GET['document'] : null;
+                        $email = isset($_GET['email']) ? $_GET['email'] : null;
+                        $telephone = isset($_GET['telephone']) ? $_GET['telephone'] : null;
+                        $cellphone = isset($_GET['cellphone']) ? $_GET['cellphone'] : null;
+                        $expertise = isset($_GET['expertise']) ? $_GET['expertise'] : null;
+                     ?>
 
                         <form class="row g-3" action="services/register.php" method="post">
                             <div class="col-12 mb-2 form-subtitle">
                                 <h4>Dados Básicos</h4>
                             </div>
                             <div class="mb-3 col-12 col-md-12">
-                                <label for="document" class="form-label">CRO <span class="text-danger">*</span></label>
-                                <input type="text" class="form-control" id="document" name="document" aria-describedby="croHelp" required>
-                                <div id="croHelp" class="form-text">Insira somente os números.</div>
-                            </div>
-                            <div class="mb-3 col-6 col-md-6">
-                                <label for="name" class="form-label">Nome <span class="text-danger">*</span></label>
-                                <input type="text" class="form-control" id="name" name="name" required>
-                            </div>
-                            <div class="mb-3 col-6 col-md-6">
-                                <label for="surname" class="form-label">Sobrenome <span class="text-danger">*</span></label>
-                                <input type="text" class="form-control" id="surname" name="surname" required>
+                                <label for="name" class="form-label">Nome</label>
+                                <input type="text" class="form-control" id="name" name="name" value="<?= $name ?>">
                             </div>
                             <div class="mb-3 col-12 col-md-12">
-                                <label for="email" class="form-label">Email <span class="text-danger">*</span></label>
-                                <input type="email" class="form-control" id="email" name="email" required>
+                                <label for="surname" class="form-label">Sobrenome</label>
+                                <input type="text" class="form-control" id="surname" name="surname" value="<?= $surname ?>">
+                            </div>
+                            <div class="mb-3 col-6 col-md-6">
+                                <label for="document" class="form-label">CRO</label>
+                                <input type="text" class="form-control" id="document" name="document" value="<?= $document ?>">
+                            </div>
+                            <div class="mb-3 col-12 col-md-12">
+                                <label for="email" class="form-label">Email </label>
+                                <input type="email" class="form-control" id="email" name="email" value="<?= $email ?>">
                             </div>
                             <div class="mb-3 col-6 col-md-6">
                                 <label for="telephone" class="form-label">Telefone</label>
-                                <input type="text" class="form-control" id="telephone" name="telephone" aria-describedby="telephoneHelp">
-                                <div id="telephoneHelp" class="form-text">Insira somente os números.</div>
+                                <input type="text" class="form-control" id="telephone" name="telephone" value="<?= $telephone ?>">
                             </div>
                             <div class="mb-3 col-6 col-md-6">
-                                <label for="cellphone" class="form-label">Celular <span class="text-danger">*</span></label>
-                                <input type="text" class="form-control" id="cellphone" name="cellphone" aria-describedby="cellphoneHelp" required>
-                                <div id="cellphoneHelp" class="form-text">Insira somente os números.</div>
+                                <label for="cellphone" class="form-label">Celular</label>
+                                <input type="text" class="form-control" id="cellphone" name="cellphone" value="<?= $cellphone ?>">
+                            </div>
+                            <div class="mb-3 col-6 col-md-6">
+                                <label for="expertise" class="form-label">Especialidade </label>
+                                <input type="text" class="form-control" id="expertise" name="expertise" value="<?= $expertise ?>">
                             </div>
                             <div class="mb-3 col-12 col-md-12">
-                                <label for="expertise" class="form-label">Especialidade <span class="text-danger">*</span></label>
-                                <input type="text" class="form-control" id="expertise" name="expertise" required>
-                            </div>
-                            <div class="mb-3 col-12 col-md-12">
-                                <label for="password" class="form-label">Senha <span class="text-danger">*</span></label>
-                                <input type="password" class="form-control" id="password" name="password" required>
+                                <label for="password" class="form-label">Senha</label>
+                                <input type="password" class="form-control" id="password" name="password">
                             </div>
 
                             <div class="col-12 btn-toolbar flex-row-reverse">
-                                <button type="submit" class="btn btn-primary" name="registerDentist">Cadastrar</button>
+                                <button type="submit" class="btn btn-primary" name="updateDentist">Cadastrar</button>
                             </div>
                         </form>
 
--- dentist-list.php
+++ dentist-list.php
@@ -1,4 +1,4 @@
-<?php include_once 'services/verificar-token.php' ?>
+<?php include_once 'services/verificar-token.php'; $status = isset($_GET['status']) ? $_GET['status'] : true?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -22,6 +22,10 @@
 
     <!-- Custom styles for this template-->
     <link href="assets/css/sb-admin-2.css" rel="stylesheet">
+    <link href="assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+    <!-- Sweet Alert -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.4.8/dist/sweetalert2.all.min.js"></script>
 
 </head>
 
@@ -127,47 +131,52 @@
 
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
-                        <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                        <div class="d-flex card-header py-3 justify-content-between">
+                            <h6 class="m-0 font-weight-bold text-primary">Listagem de Dentistas</h6>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="disabledProcedures" onchange="status()" <?= isset($_GET['status']) ? 'checked' : '' ?>>
+                                <label class="form-check-label" for="disabledProcedures">
+                                    Exibir dentistas não ativos
+                                </label>
+                            </div>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
-                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                <table class="table table-bordered table-hover" id="dataTable" width="100%" cellspacing="0">
                                     <thead>
                                         <tr>
-                                            <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>
-                                            <th>Start date</th>
-                                            <th>Salary</th>
+                                            <th>Nome</th>
+                                            <th>Sobrenome</th>
+                                            <th>Email</th>
+                                            <th>Expertise</th>
+                                            <th class="text-center" style="width:150px">Ações</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <td>Tiger Nixon</td>
-                                            <td>System Architect</td>
-                                            <td>Edinburgh</td>
-                                            <td>61</td>
-                                            <td>2011/04/25</td>
-                                            <td>$320,800</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Garrett Winters</td>
-                                            <td>Accountant</td>
-                                            <td>Tokyo</td>
-                                            <td>63</td>
-                                            <td>2011/07/25</td>
-                                            <td>$170,750</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Ashton Cox</td>
-                                            <td>Junior Technical Author</td>
-                                            <td>San Francisco</td>
-                                            <td>66</td>
-                                            <td>2009/01/12</td>
-                                            <td>$86,000</td>
-                                        </tr>
+                                        <?php
+                                            include_once 'services/requestAPI.php';
+                                            $json = requestApi('GET', 'http:/localhost:8080/dentists/list/' . $status, false, $_SESSION['token']);
+                                            $data = json_decode($json);
+
+                                            foreach ($data as $key => $value){
+                                                if ($value->status){
+                                                    $workaround = "<a href='#' class='btn btn-sm btn-danger' data-toggle='modal' data-target='#removeModal" . $value->userId . "'><i class='fas fa-trash'></i></a>";
+                                                } else {
+                                                    $workaround = "";
+                                                }
+                                                echo "<tr>
+                                                        <td> " . $value->name . " </td>
+                                                        <td> " . $value->surname . " </td>
+                                                        <td> " . $value->email . " </td>
+                                                        <td> " . $value->expertise . " </td>
+                                                        <td class='text-center'> 
+                                                            <a href='#' class='btn btn-sm btn-primary' data-toggle='modal' data-target='#detailModal" . $value->userId . "'><i class='fas fa-eye'></i></a>
+                                                            <a href='dentist-edit.php?id=" . $value->userId . "&name=" . $value->name . "&surname=" . $value->surname . "&document=" . $value->document . "&email=" . $value->email . "&telephone=" . $value->telephone . "&cellphone=" . $value->cellphone . "&expertise=" . $value->expertise . "' class='btn btn-sm btn-warning'><i class='fas fa-pen'></i></a>
+                                                            " . $workaround . "
+                                                        </td>
+                                                    </tr>";
+                                            }
+                                        ?>
                                     </tbody>
                                 </table>
                             </div>
@@ -221,6 +230,114 @@
         </div>
     </div>
 
+    <!-- Detail Modal-->
+    <?php
+        include_once 'services/requestAPI.php';
+        $json = requestApi('GET', 'http:/localhost:8080/dentists/list/' . $status, false, $_SESSION['token']);
+        $data = json_decode($json);
+
+        foreach ($data as $key => $value){
+            echo "
+                <div class='modal fade' id='detailModal" . $value->userId . "' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel'
+                    aria-hidden='true'>
+                    <div class='modal-dialog' role='document'>
+                        <div class='modal-content'>
+                            <div class='modal-header'>
+                                <h5 class='modal-title' id='exampleModalLabel'>Detalhes do Dentista</h5>
+                                <button class='close' type='button' data-dismiss='modal' aria-label='Close'>
+                                    <span aria-hidden='true'>×</span>
+                                </button>
+                            </div>
+                            <div class='modal-body'>
+                                <form class='row g-3'>
+
+                                <div class='mb-3 col-6 col-md-6'>
+                                    <label for='name' class='form-label'>Nome</label>
+                                    <input type='text' class='form-control' id='name' value='" . $value->name . "' disabled>
+                                </div>
+                                <div class='mb-3 col-6 col-md-6'>
+                                    <label for='surname' class='form-label'>Sobrenome</label>
+                                    <input type='text' class='form-control' id='surname' value='" . $value->surname . "' disabled>
+                                </div>
+                                <div class='mb-3 col-12 col-md-12'>
+                                    <label for='document' class='form-label'>CRO</label>
+                                    <input type='text' class='form-control' id='document' value='" . $value->document . "' disabled>
+                                </div>
+                                <div class='mb-3 col-12 col-md-12'>
+                                    <label for='expertise' class='form-label'>Expertise</label>
+                                    <input type='text' class='form-control' id='expertise' value='" . $value->expertise . "' disabled>
+                                </div>
+                                <div class='mb-3 col-12 col-md-12'>
+                                    <label for='email' class='form-label'>Email</label>
+                                    <input type='email' class='form-control' id='email' value='" . $value->email . "' disabled>
+                                </div>
+                                <div class='mb-3 col-12 col-md-6'>
+                                    <label for='telephone' class='form-label'>Telefone</label>
+                                    <input type='text' class='form-control' id='telephone' value='" . $value->telephone . "' disabled>
+                                </div>
+                                <div class='mb-3 col-12 col-md-6'>
+                                    <label for='cellphone' class='form-label'>Celular</label>
+                                    <input type='text' class='form-control' id='cellphone' value='" . $value->cellphone . "' disabled>
+                                </div>
+                                </form>
+                            </div>
+                            <div class='modal-footer'>
+                                <a href='dentist-edit.php?id=" . $value->userId . "&name=" . $value->name . "&surname=" . $value->surname . "&document=" . $value->document . "&email=" . $value->email . "&telephone=" . $value->telephone . "&cellphone=" . $value->cellphone . "&expertise=" . $value->expertise. "' class='btn btn-primary'>Editar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>";
+        }
+    ?>
+
+    <!-- Remove Modal-->
+    <?php
+        include_once 'services/requestAPI.php';
+        $json = requestApi('GET', 'http:/localhost:8080/dentists/list/' . $status, false, $_SESSION['token']);
+        $data = json_decode($json);
+
+        foreach ($data as $key => $value){
+            echo "
+                <div class='modal fade' id='removeModal" . $value->userId . "' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel'
+                    aria-hidden='true'>
+                    <div class='modal-dialog' role='document'>
+                        <div class='modal-content'>
+                            <div class='modal-header'>
+                                <h5 class='modal-title' id='exampleModalLabel'>Tem certeza que quer exluir esse registro?</h5>
+                                <button class='close' type='button' data-dismiss='modal' aria-label='Close'>
+                                    <span aria-hidden='true'>×</span>
+                                </button>
+                            </div>
+                            <div class='modal-body'>Selecione 'Sim' para confirmar a exclusão.</div>
+                            <div class='modal-footer'>
+                                <form action='' method='post'>
+                                    <button class='btn btn-secondary' type='button' data-dismiss='modal'>Não</button>
+                                    <button class='btn btn-primary' type='submit' name='confirmDeletion'>Sim</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>";
+        }
+        if (isset($_POST['confirmDeletion'])){
+            $postData = array (
+                "userId" => $value->userId,
+                "document" => $value->userId,
+                "name" => $value->name,
+                "surname" => $value->surname,
+                "email" => $value->email,
+                "telephone" => $value->telephone,
+                "cellphone" => $value->cellphone,
+                "expertise" => $value->expertise,
+                "password" => $value->password,
+            );
+
+            $response = requestApi('POST', 'http:/localhost:8080/dentists/remove', $postData, $_SESSION['token']);
+
+            echo "<script> window.location = 'dentist-list.php?response=" . $response . "' </script>";
+        }
+    ?>
+
     <!-- Bootstrap core JavaScript-->
     <script src="assets/vendor/jquery/jquery.min.js"></script>
     <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -232,12 +349,50 @@
     <script src="assets/js/sb-admin-2.min.js"></script>
 
     <!-- Page level plugins -->
-    <script src="assets/vendor/chart.js/Chart.min.js"></script>
+    <script src="assets/vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>    
 
     <!-- Page level custom scripts -->
-    <script src="assets/js/demo/chart-area-demo.js"></script>
-    <script src="assets/js/demo/chart-pie-demo.js"></script>
+    <script src="assets/js/demo/datatables-demo.js"></script>
+
+    <script>
+        function status(){
+            if (<?= $status ?> == false){
+                window.location='dentist-list.php';
+            } else {
+                window.location='dentist-list.php?status=false';
+            }
+        }
+    </script>
 
 </body>
 
 </html>
+
+<?php
+
+    $response = isset($_GET['response']) ? $_GET['response'] : null;
+
+    if (isset($response)){
+        if ($response == '200 OK'){
+            echo "<script>
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Sucesso!',
+                    text: 'Operação realizada com sucesso!',
+                    confirmButtonUrl: 'dentist-list.php'
+                })
+            </script>";
+        } else {
+            echo "<script>
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Algo deu errado ao cadastrar o procedimento!',
+                    confirmButtonUrl: 'dentist-list.php'
+                })
+            </script>";
+        }
+    }
+
+?>
